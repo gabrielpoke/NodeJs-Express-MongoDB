7c1694e76c3895ff9a844421ff7234a29c59dee2
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2023-01-04 17:38:35 -0300

    Criando um Crud

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
 const server = express()
+
+//Comando indicando que estamos recebendo e trabalhando com obejtos JSON
+server.use(express.json())
+
 //Query params - parametros que passamos na frente da roda
 
 //Route params - paramentos de acesso enviado via rota
@@ -9,16 +13,45 @@ const server = express()
 
 const cursos = ['NodeJs', 'JavaScript', 'React Native']
 
+//Pega todos os cursos do Array
+server.get('/cursos', (req, res) => {
+  return res.json(cursos)
+})
+
+//Pega curso referenciando o index
 server.get('/cursos/:index', (req, res) => {
   const { index } = req.params
 
   return res.json(cursos[index])
 })
 
-server.get('/cursos', (req, res) => {
-  const { nome } = req.query
+//Salvando um novo curso
+server.post('/cursos', (req, res) => {
+  const { nome } = req.body
 
-  return res.json({ nome: `Nome do curso ${nome}` })
+  cursos.push(nome)
+
+  return res.json(cursos)
+})
+
+//Atualizando cursos
+server.put('/cursos/:index', (req, res) => {
+  const { index } = req.params
+
+  const { nome } = req.body
+
+  cursos[index] = nome
+
+  return res.json(cursos)
+})
+
+//Deletando cursos
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params
+
+  cursos.splice(index, 1)
+
+  return res.json(cursos)
 })
 
 //localhost:3000
