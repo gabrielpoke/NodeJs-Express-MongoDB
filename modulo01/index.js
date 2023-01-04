280c9c44a3114599dc222c062dc44effe23399ec
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2023-01-04 16:52:34 -0300

    Entendendo query e router params

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 
 const server = express()
+//Query params - parametros que passamos na frente da roda
 
-server.get('/cursos', (req, res) => {
+//Route params - paramentos de acesso enviado via rota
+
+//Request Body - requisição de obejto contendo informações
+
+server.get('/cursos/:id', (req, res) => {
+  const id = req.params.id
+
+  const nome = req.query.nome
+
   return res.json({
-    curso: 'Node.Js'
+    cursos: `Nome do curso ${nome}`,
+    id: `Id do curso ${id}`
   })
 })
 
