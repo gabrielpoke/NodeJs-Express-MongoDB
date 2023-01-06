6677bfdc451f325beeeb3bf70019bccda6064c31
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2023-01-05 22:05:46 -0300

    Trabalhando com MongoDB

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,26 @@
+//yarn add express -D
 import express from 'express'
+
+//yarn add mongoose -D
+import mongoose from 'mongoose'
+
 import routes from './routes'
 
 class App {
   constructor() {
     this.server = express()
+
+    //Remove o warning do mongoose
+    mongoose.set('strictQuery', true)
+
+    //Parte de conecção com o Banco MongoDB
+    mongoose.connect(
+      'mongodb+srv://devhouse:devhouse@devhouse.do5qb8h.mongodb.net/devhouse?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
     this.middlewares()
     this.routes()
   }
